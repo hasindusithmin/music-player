554fb3229d052f34ff801c8cd81d060feba55cee
hasindu sithmin <hasindusithmin64@gmail.com>
2022-12-25 08:11:13 +0000

    Create 2 src/songs.json,src/singers.json & Update src/App.js,src/index.css,README.md & Delete src/App.css

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # music-player
 
-Welcome to the ReactJS and AmplitudeJS music player! This application was built using the latest technologies in order to provide you with an intuitive and seamless audio experience.
+Welcome to the ReactJS music player! This application was built using the latest technologies in order to provide you with an intuitive and seamless audio experience.
 
 With our music player, you'll be able to browse and play your favorite tracks using the SongAPI. And with the added PWA (Progressive Web App) functionality, you'll be able to use this application on the go without the need for an internet connection.
 
--- src/App.js
+++ src/App.js
@@ -1,24 +1,97 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import songs from "./songs.json"
+import singers from "./singers.json"
+import autoComplete from "@tarekraafat/autocomplete.js";
+
 
 function App() {
+
+  useEffect(() => {
+    const autoCompleteSongs = new autoComplete({
+      selector: "#autocomplete-songs",
+      placeHolder: "Search for Songs...",
+      data: {
+        src: songs.map(({ song }) => song),
+        cache: true,
+      },
+      resultsList: {
+        element: (list, data) => {
+          if (!data.results.length) {
+            // Create "No Results" message element
+            const message = document.createElement("div");
+            // Add class to the created element
+            message.setAttribute("class", "no_result");
+            // Add message text content
+            message.innerHTML = `<span>Found No Results for "${data.query}"</span>`;
+            // Append message element to the results list
+            list.prepend(message);
+          }
+        },
+        noResults: true,
+      },
+      resultItem: {
+        highlight: true
+      },
+      events: {
+        input: {
+          selection: (event) => {
+            const selection = event.detail.selection.value;
+            autoCompleteSongs.input.value = selection;
+          }
+        }
+      }
+    });
+    const autoCompleteSingers = new autoComplete({
+      selector: "#autocomplete-singers",
+      placeHolder: "Search for Singers...",
+      data: {
+        src: singers,
+        cache: true,
+      },
+      resultsList: {
+        element: (list, data) => {
+          if (!data.results.length) {
+            // Create "No Results" message element
+            const message = document.createElement("div");
+            // Add class to the created element
+            message.setAttribute("class", "no_result");
+            // Add message text content
+            message.innerHTML = `<span>Found No Results for "${data.query}"</span>`;
+            // Append message element to the results list
+            list.prepend(message);
+          }
+        },
+        noResults: true,
+      },
+      resultItem: {
+        highlight: true
+      },
+      events: {
+        input: {
+          selection: (event) => {
+            const selection = event.detail.selection.value;
+            autoCompleteSingers.input.value = selection;
+          }
+        }
+      }
+    });
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='w3-content w3-panel w3-center'>
+      <div className='w3-row'>
+        <div className='w3-half'>
+          <div className="autoComplete_wrapper">
+            <input id="autocomplete-songs" type="search" dir="ltr" spellCheck={false} autoCorrect="off" autoComplete="off" autoCapitalize="off" />
+          </div>
+        </div>
+        <div className='w3-half'>
+          <div className="autoComplete_wrapper">
+            <input id="autocomplete-singers" type="search" dir="ltr" spellCheck={false} autoCorrect="off" autoComplete="off" autoCapitalize="off" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
